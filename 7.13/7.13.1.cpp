3d392860ee35edf4bf4cd9450ee2ef1919a22e2c
magic-zhu <18817313320@163.com>
2022-09-04 22:19:38 +0800

    update

--- 7.13/7.13.1.cpp
+++ 7.13/7.13.1.cpp
@@ -1,6 +1,29 @@
-#include <Windows.h>
+// 1.编写一个程序，不断要求用户输入两个数，直到其中的一个为0。对于每两个数，程序将使用一个
+// 函数来计算它们的调和平均数，并将结果返回给main(),而后者将报告结果。调和平均数指的是倒数平均
+// 值的倒数，计算公式如下：
+// 调和平均数=2.0*x*y/(x+y)
 
-int main() {
-	
-	system("pause");
+#include <Windows.h>
+#include <iostream>
+
+using namespace std;
+
+double cau(int a, int b)
+{
+	return 2 * (a * b) / (a + b);
 }
+
+int main()
+{
+	int a;
+	int b;
+	cout << "请输入两个数，以空格分隔：";
+	while ((cin >> a >> b) && a != 0 && b != 0)
+	{
+		const double r = cau(a, b);
+		cout << "调和平均数 = " << r << endl;
+		cout << "请输入两个数，以空格分隔：";
+	}
+	system("pause");
+};
+
---
+++ 7.13/7.13.2.cpp
@@ -0,0 +1,57 @@
+// 2.编写一个程序，要求用户输入最多10个高尔夫成绩，并将其存储在一个数组中。程序允许用户提
+// 早结束输入，并在一行上显示所有成绩，然后报告平均成绩。请使用3个数组处理函数来分别进行输入、
+// 显示和计算平均成绩。
+
+#include <iostream>
+#include <string.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int inputList(int list[])
+{
+    int len = 0;
+    cin >> list[len];
+    while (cin.get() != '\n' && len < 9)
+    {
+        len++;
+        cin >> list[len];
+    }
+    return len + 1;
+}
+
+void showList(int list[], const int len)
+{
+    for (int Index = 0; Index < len; Index++)
+    {
+        cout << list[Index] << " ";
+    }
+}
+
+void avgList(int list[], const int len)
+{
+    double sum = 0;
+    for (int Index = 0; Index < len; Index++)
+    {
+        sum += list[Index];
+    }
+    if (len != 0)
+    {
+        cout << "平均成绩为:" << sum / len;
+    }
+    else
+    {
+        cout << "平均成绩为:" << 0;
+    }
+}
+
+int main()
+{
+    int list[10];
+    int len = 0;
+    len = inputList(list);
+    showList(list, len);
+    avgList(list, len);
+    system("pause");
+    return 0;
+};
